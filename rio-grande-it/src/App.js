4802173fc38a082f7d6cd06585a83634283a2f5a
Esteban Gueicha <estebnauquito181@gmail.com>
2020-12-08 14:17:17 -0300

    banner-scaffolding

--- rio-grande-it/src/App.js
+++ rio-grande-it/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import { Banner } from './Components/Banner/Banner';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Banner />
     </div>
   );
 }
--- rio-grande-it/src/Components/Banner/Banner.js
+++ rio-grande-it/src/Components/Banner/Banner.js
@@ -1,1 +1,21 @@
+import React from 'react';
 import './Banner.css'
+import { Container, Row, Col, Button } from 'react-bootstrap'
+import { CountDown } from '../CountDown'
+
+export const Banner = () => {
+    return (
+        <Container className='banner-container'>
+            <Row>
+                <Col lg={6}>
+                    <div className='principal-text-container'>
+                        <h1>Congreso de Industrias Tecnológicas</h1>
+                        <p>mucho texto</p>
+                        <CountDown />
+                        <Button>Incribite</Button>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
