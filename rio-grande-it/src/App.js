7f2784aa514238690ed69f2b9c8a3cfd1c08e870
afgamalerio <anahifernandezg@gmail.com>
2020-12-08 16:02:58 -0300

    Cambios

--- rio-grande-it/src/App.js
+++ rio-grande-it/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Banner } from './Components/Banner/Banner';
-
+import { Header } from './Components/Header/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Banner />
     </div>
   );
--- rio-grande-it/src/Components/Banner/Banner.js
+++ rio-grande-it/src/Components/Banner/Banner.js
@@ -5,14 +5,14 @@ import { CountDown } from '../CountDown'
 
 export const Banner = () => {
     return (
-        <Container className='banner-container'>
+        <Container bsPrefix='banner-container'>
             <Row>
-                <Col lg={6}>
+                <Col lg={12}>
                     <div className='principal-text-container'>
-                        <h1>Congreso de Industrias Tecnológicas</h1>
-                        <p>mucho texto</p>
+                        <h1 className='principal-title'>Congreso de Industrias Tecnológicas</h1>
+                        <p className='principal-text'>Conocé la industria tecnológica en Rio Grande e involucrate en este movimiento imparable que es el IT. Participá de 3 días de charlas, paneles y talleres.</p>
                         <CountDown />
-                        <Button>Incribite</Button>
+                        <Button className='cta'>Inscribirse</Button>
                     </div>
                 </Col>
             </Row>
