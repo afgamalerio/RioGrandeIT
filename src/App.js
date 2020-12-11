81ef181d414ad1d8f6142f13cf1d151626db4942
afgamalerio <anahifernandezg@gmail.com>
2020-12-11 20:50:33 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Banner } from './Components/Banner';
 import { Header } from './Components/Header';
-import { SpeakersAgenda } from './Components/SpeakersAgenda';
 import { Sponsors } from './Components/Sponsors'
 import { Contact } from './Components/Contact'
 import { Footer } from './Components/Footer'
 import { Mark } from './Components/Mark';
-import { Link } from 'react-scroll';
-import { Button } from 'react-bootstrap';
 
 function App() {
   return (
@@ -16,19 +13,11 @@ function App() {
       <Header />
       <main>
         <Banner />
-        <SpeakersAgenda />
         <Sponsors />
         <Contact />
       </main>
       <Footer />
       <Mark />
-      <Link to='home' smooth={true}>
-        <Button className='home-button'>
-          <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-caret-up-fill" fill="#ffffff" xmlns="http://www.w3.org/2000/svg">
-            <path d="M7.247 4.86l-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
-          </svg>
-        </Button>
-      </Link>
     </div>
   );
 }
