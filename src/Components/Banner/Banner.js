37b730ba0652a7dd393c33818b8daf5bf5e6fd94
afgamalerio <anahifernandezg@gmail.com>
2020-12-18 14:15:30 -0300

    Video link update

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -6,7 +6,7 @@ import ReactPlayer from 'react-player'
 
 export const Banner = () => {
     const [isFinish, setIsFinish] = useState(false)
-    const YoutubeUrl = 'https://www.youtube.com/watch?v=5qap5aO4i9A&ab_channel=ChilledCow'
+    const YoutubeUrl = 'https://www.youtube.com/watch?v=h3pkeICnRlY&feature=youtu.be&ab_channel=EspacioTecnol%C3%B3gicoRGA'
 
     return (
         <section id="inicio">
