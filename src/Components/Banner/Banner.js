d1277260dbfc64a3ff42384642ff39082d95e274
Esteban Gueicha <estebnauquito181@gmail.com>
2020-12-18 13:50:10 -0300

    fix banner logic

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -5,38 +5,33 @@ import { CountDown } from '../CountDown'
 import ReactPlayer from 'react-player'
 
 export const Banner = () => {
-    const [timer, setTimer] = useState([])
+    const [isFinish, setIsFinish] = useState(false)
     const YoutubeUrl = 'https://www.youtube.com/watch?v=5qap5aO4i9A&ab_channel=ChilledCow'
-    const handleTimer = (t) => {
-        setTimer(t)
-    }
-    console.log(timer)
+
     return (
         <section id="inicio">
             <Container bsPrefix='banner-container'>
-                {Object.keys(timer).length ? (
+                {!isFinish ? (
                     <Container className='box-title'>
                         <Row>
                             <Col lg={11}>
-
                                 <div className='principal-text-container'>
                                     <h1 className='principal-title'>Congreso de <br /> Industrias Tecnológicas</h1>
                                     <p className='principal-text'>Conocé la industria tecnológica en Rio Grande e involucrate en este movimiento imparable que es el IT. Participá de 3 días de charlas, paneles y talleres.</p>
-                                    <CountDown setTimer={handleTimer} />
+                                    <CountDown setIsFinish={setIsFinish} />
                                     <Button className='cta' onClick={() => window.open('http://bit.ly/RioGrandeIT')}>Inscribirse</Button>
                                 </div>
                             </Col>
                         </Row>
                     </Container>
-                ) :
-                    <Container className='video-container'>
-                        <ReactPlayer
-                            controls
-                            url={YoutubeUrl}
-                        />
-                    </Container>
-
-                }
+                ) : (
+                        <Container className='video-container'>
+                            <ReactPlayer
+                                controls
+                                url={YoutubeUrl}
+                            />
+                        </Container>
+                    )}
             </Container>
         </section>
     );
